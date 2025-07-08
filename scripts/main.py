c30cd740466610bac102ae7b3d849688784dc6c5
zigzag2425 <etienne.sebille@gmail.com>
2025-07-08 17:58:34 +0200

    Create main.py

---
+++ scripts/main.py
@@ -0,0 +1,9 @@
+import os
+
+os.system("python scripts/1_generate_kick.py")
+os.system("python scripts/2_generate_bassline.py")
+os.system("python scripts/3_generate_lead.py")
+os.system("python scripts/4_generate_break.py")
+os.system("python scripts/5_arrange_track.py")
+os.system("python scripts/6_auto_mix.py")
+os.system("python scripts/7_export_als.py")
